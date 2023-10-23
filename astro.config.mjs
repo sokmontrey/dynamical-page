a5b30c912fd5a49c150b19be82df415095df1c59
Sokmontrey <sythatsokmontrey@gmail.com>
2023-10-23 18:14:12 -0400

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
