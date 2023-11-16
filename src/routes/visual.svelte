80ae881735dae17363438a700591f06f09515b20
Sokmontrey <sythatsokmontrey@gmail.com>
2023-11-15 23:04:05 -0500

    new quick note

--- src/routes/visual.svelte
+++ src/routes/visual.svelte
@@ -6,6 +6,15 @@
     <IntroSim />
 </div>
 
+<div visual='quick-note' class='w-1/2 pr-10'>
+    <strong>
+        <i class="fa-solid fa-triangle-exclamation mx-5"></i>
+    </strong>
+    <p>
+        Please <strong>keep in mind</strong> that this was created for my learning purpose only. There are a lot of rooms for improvement.
+    </p>
+</div>
+
 <div visual='point'>
     Point
 </div>
